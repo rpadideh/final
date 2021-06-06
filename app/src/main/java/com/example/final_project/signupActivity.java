a2b51faef2459dcf759bc7f67696a0913daa16ff
Padideh <padideh.rayenii@gmail.com>
2021-06-06 16:04:17 +0430

    getter & setter

--- app/src/main/java/com/example/final_project/signupActivity.java
+++ app/src/main/java/com/example/final_project/signupActivity.java
@@ -3,12 +3,41 @@ package com.example.final_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class signupActivity extends AppCompatActivity {
+
+    FirebaseAuth auth;
+    EditText email,password,name;
+    Button login2,signup2;
+
+    FirebaseFirestore  database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        database=FirebaseFirestore.getInstance();
+        auth=FirebaseAuth.getInstance();
+
+        email=findViewById(R.id.txtemail2);
+        password=findViewById(R.id.txtpass2);
+        name=findViewById(R.id.txtname);
+
+        login2=findViewById(R.id.btnlogin2);
+        signup2=findViewById(R.id.btnsignup2);
+
+        signup2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
